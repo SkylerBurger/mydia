2f1dc901ecf9f287860fe5685a7d5272c4dbd3aa
Skyler Burger <skylerburger@gmail.com>
2019-10-17 16:53:59 -0700

    Register Movie app in admin

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField('title', max_length=200)
+    release_year = models.IntegerField('release year')
+    mpaa_rating = models.CharField('MPAA rating', max_length=5)
+    run_time = models.IntegerField('run time')
+    platform = models.CharField('platform', max_length=200)
+    form = models.CharField('format', max_length=200)
+
+    def __str__(self):
+        return f'{self.title} ({self.release_year})'
