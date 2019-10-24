227527ce33b8438e62c9d31ed2118ff2e8f1104e
Skyler Burger <skylerburger@gmail.com>
2019-10-24 11:24:51 -0700

    Add MovieCopy model

--- movies/models.py
+++ movies/models.py
@@ -6,8 +6,15 @@ class Movie(models.Model):
     release_year = models.IntegerField('release year')
     mpaa_rating = models.CharField('MPAA rating', max_length=5)
     run_time = models.IntegerField('run time')
+
+    def __str__(self):
+        return f'{self.title} ({self.release_year})'
+
+
+class MovieCopy(models.Model):
+    film = models.ForeignKey(Movie, on_delete=models.CASCADE)
     platform = models.CharField('platform', max_length=200)
     form = models.CharField('format', max_length=200)
 
     def __str__(self):
-        return f'{self.title} ({self.release_year})'
+        return f'{self.form} of {self.film} on {self.platform}'
